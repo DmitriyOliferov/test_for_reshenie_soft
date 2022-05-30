d82be4aa2b7b516dd7962fa97f57f96c35e956ca
Dmitriy Oliferov <DrEfrit@yandex.ru>
2022-05-30 17:00:42 +0300

    Initial commit

---
+++ app/src/main/java/com/oliferov/test_for_reshenie_soft/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oliferov.test_for_reshenie_soft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
