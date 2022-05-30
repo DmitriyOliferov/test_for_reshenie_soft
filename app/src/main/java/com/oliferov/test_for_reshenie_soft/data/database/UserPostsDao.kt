65f3e0bcbbc9a54d2351f790ea33b4b1c8b7718f
Dmitriy Oliferov <DrEfrit@yandex.ru>
2022-05-30 19:26:39 +0300

    create RepositoryImpl

--- app/src/main/java/com/oliferov/test_for_reshenie_soft/data/database/UserPostsDao.kt
+++ app/src/main/java/com/oliferov/test_for_reshenie_soft/data/database/UserPostsDao.kt
@@ -1,6 +1,8 @@
 package com.oliferov.test_for_reshenie_soft.data.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.oliferov.test_for_reshenie_soft.data.database.DbModel.NewsDbModel
 import com.oliferov.test_for_reshenie_soft.data.database.DbModel.UserDbModel
@@ -9,8 +11,14 @@ import com.oliferov.test_for_reshenie_soft.data.database.DbModel.UserDbModel
 interface UserPostsDao {
 
     @Query("SELECT * FROM users ORDER BY id DESC")
-        fun getAllUsers(): List<UserDbModel>
+    fun getAllUsers(): List<UserDbModel>
 
-        @Query("SELECT * FROM news WHERE id == :uid")
-        fun getAllUserNews(uid: Int): List<NewsDbModel>
+    @Query("SELECT * FROM news WHERE id == :uid")
+    fun getAllUserNews(uid: Int): List<NewsDbModel>
+
+    @Insert(entity = UserDbModel::class, onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllUsers(users: List<UserDbModel>)
+
+    @Insert(entity = NewsDbModel::class, onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllNews(news: List<NewsDbModel>)
 }
---
+++ app/src/main/java/com/oliferov/test_for_reshenie_soft/data/repository/UserPostsRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package com.oliferov.test_for_reshenie_soft.data.repository
+
+import com.oliferov.test_for_reshenie_soft.data.database.UserPostsDao
+import com.oliferov.test_for_reshenie_soft.data.mapper.NewsMapper
+import com.oliferov.test_for_reshenie_soft.data.mapper.UserMapper
+import com.oliferov.test_for_reshenie_soft.data.network.ApiService
+import com.oliferov.test_for_reshenie_soft.domain.UserPostsRepository
+import com.oliferov.test_for_reshenie_soft.domain.model.News
+import com.oliferov.test_for_reshenie_soft.domain.model.User
+
+class UserPostsRepositoryImpl(
+    val apiService: ApiService,
+    val dao: UserPostsDao,
+    val newsMapper: NewsMapper,
+    val userMapper: UserMapper
+) : UserPostsRepository {
+
+    override suspend fun getAllUsers(): List<User> {
+        val listUserDto = apiService.getAllUsers().body() ?: emptyList()
+        val listUserDbModel = listUserDto.map {
+            userMapper.mapUserDtoToUserDbModel(it)
+        }
+        dao.insertAllUsers(listUserDbModel)
+        val listUserModel = dao.getAllUsers().map {
+            userMapper.mapUserDbModelToUserModel(it)
+        }
+        return listUserModel
+    }
+
+    override suspend fun getAllUserNews(uid: Int): List<News> {
+        val listNewsDto = apiService.getAllUserNews().body() ?: emptyList()
+        val listNewsDbModel = listNewsDto.map {
+            newsMapper.mapNewsDtoToNewsDbModel(it)
+        }
+        dao.insertAllNews(listNewsDbModel)
+        val listNewsModel = dao.getAllUserNews(uid).map {
+            newsMapper.mapNewsDbModelToNewsModel(it)
+        }
+        return listNewsModel
+    }
+}
